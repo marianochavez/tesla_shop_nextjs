ff9da7b7787c0d8fa5d61f63913b27c5a35b8259
marianochavez <amarianochavez@gmail.com>
2022-07-15 13:38:36 -0300

    db/restful S13

---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,34 @@
+import type {NextApiRequest, NextApiResponse} from "next";
+
+import {db} from "../../../database";
+import {IProduct} from "../../../interfaces";
+import Product from "../../../models/Product";
+
+type Data = {
+  message: string;
+  product?: IProduct;
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  switch (req.method) {
+    case "GET":
+      return getProductBySlug(req, res);
+
+    default:
+      return res.status(400).json({message: "Method not allowed"});
+  }
+}
+const getProductBySlug = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  await db.connect();
+
+  const {slug} = req.query;
+  const product = await Product.findOne({slug}).lean();
+
+  await db.disconnect();
+
+  if (!product) {
+    return res.status(404).json({message: "Product not found"});
+  }
+
+  res.status(200).json({message: "OK", product});
+};
--- pages/api/search/index.ts
+++ pages/api/search/index.ts
@@ -1,10 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from "next";
 
 type Data = {
-  name: string;
+  message: string;
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({name: "John Doe"});
+  res.status(400).json({message: "Method not allowed"});
 }
