0f37d3a31b9dedf57e9a4a0b2c45c667d291d859
marianochavez <amarianochavez@gmail.com>
2022-08-14 18:48:36 -0300

    next/img

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,6 @@
 import {FC, useMemo, useState} from "react";
-import {Box, Grid, Image, Link, Tag, Text} from "@chakra-ui/react";
+import NextImage from "next/image";
+import {Box, Grid, GridItem, Link, Tag, Text} from "@chakra-ui/react";
 import NextLink from "next/link";
 import {motion} from "framer-motion";
 
@@ -27,46 +28,61 @@ export const ProductCard: FC<Props> = ({product, imagesLoaded, onImageLoaded}) =
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <NextLink passHref href={`/product/${product.slug}`} prefetch={false}>
-        <Link>
-          <Box position="relative">
-            {product.inStock === 0 && (
-              <Tag
-                backgroundColor="white"
-                borderRadius="full"
+      <GridItem justifyContent="center">
+        <NextLink passHref href={`/product/${product.slug}`} prefetch={false}>
+          <Link>
+            <Box display="block" position="relative">
+              {product.inStock === 0 && (
+                <Tag
+                  backgroundColor="black"
+                  borderRadius="full"
+                  className="fadeIn"
+                  color="white"
+                  display={imagesLoaded ? "block" : "none"}
+                  fontWeight="semibold"
+                  p={2}
+                  position="absolute"
+                  right="20px"
+                  top="20px"
+                  zIndex={99}
+                >
+                  No disponible
+                </Tag>
+              )}
+              <NextImage
+                alt={product.title}
+                blurDataURL="https://res.cloudinary.com/chavedo/image/upload/v1660513375/tesla-shop/png-clipart-tesla-tesla.png"
+                height="550px"
+                layout="intrinsic"
+                placeholder="blur"
+                priority={true}
+                src={productImage}
+                width="550px"
+                onLoadingComplete={() => onImageLoaded(true)}
+              />
+              {/* <Image
+                alt={product.title}
+                as={motion.img}
+                borderRadius="10px"
                 className="fadeIn"
-                color="white"
-                fontWeight="semibold"
-                p={2}
-                position="absolute"
-                right="20px"
-                top="20px"
-                zIndex={99}
-              >
-                No disponible
-              </Tag>
-            )}
-            <Image
-              alt={product.title}
-              as={motion.img}
-              borderRadius="10px"
-              className="fadeIn"
-              height="calc(100% - 20px)"
-              position="relative"
-              src={productImage}
-              // @ts-ignore
-              whileInView={() => onImageLoaded(true)}
-              width="100%"
-              // onLoad={() => onImageLoaded(true)}
-            />
-          </Box>
-        </Link>
-      </NextLink>
+                height="350px"
+                loading="lazy"
+                position="relative"
+                src={productImage}
+                // @ts-ignore
+                whileInView={() => onImageLoaded(true)}
+                width="100%"
+                // onLoad={() => onImageLoaded(true)}
+              /> */}
+            </Box>
+          </Link>
+        </NextLink>
 
-      <Box className="fadeIn" display={imagesLoaded ? "block" : "none"} mt={1}>
-        <Text fontWeight={700}>{product.title}</Text>
-        <Text fontWeight={500}>{`$${product.price}`}</Text>
-      </Box>
+        <Box className="fadeIn" display={imagesLoaded ? "block" : "none"} mt={1}>
+          <Text fontWeight={700}>{product.title}</Text>
+          <Text fontWeight={500}>{`$${product.price}`}</Text>
+        </Box>
+      </GridItem>
     </Grid>
   );
 };
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["https://res.cloudinary.com/chavedo/", "localhost", process.env.HOST_NAME],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
